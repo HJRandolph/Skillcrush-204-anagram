08073f9c68b260138d98db8fb928a17df8dc79cd
HJRandolph <hjrandolph@gmail.com>
2016-12-30 12:49:51 -0500

    adding words_controler

--- app/controllers/words_controller.rb
+++ app/controllers/words_controller.rb
@@ -1,4 +1,15 @@
 get '/words' do
-	@words = Word.all
+	@word = Word.all
 	erb :"/words/index"
 end
+
+get '/words/:word' do
+	@word = Word.find(params[:word])
+	erb :"/words/show"
+end
+
+get '/words/:id' do
+	@word = Word.find(params[:id])
+	erb :"/words/show"
+end
+
