654662bcc3ebc0fa49927be938149dc5f310d3e1
HJRandolph <hjrandolph@gmail.com>
2017-01-16 18:34:12 -0500

    Adding new changes

--- app/controllers/index_controller.rb
+++ app/controllers/index_controller.rb
@@ -56,8 +56,9 @@ end
 
 get '/anagrams/:word' do
 	@word = params[:word]
-	alphabetized_string = @word.chars.sort.join
-	@anagrams = Word.where("letters=?",alphabetized_string)
+	word_array = @word.chars.sort
+	alphabetized_string = word_array.join
+	@anagrams = Word.where("letters=?", alphabetized_string)
 	erb :show
 end
 
