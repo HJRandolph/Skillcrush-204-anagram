58945dbf4414b1eeac447b097dd7d60ab794147c
HJRandolph <hjrandolph@gmail.com>
2017-01-16 20:19:28 -0500

    Made changes to words_controller.rb trying to get index to work. Also changed words_create_spec so that it looks for a word that is in the dictionary when testing.

--- app/controllers/words_controller.rb
+++ app/controllers/words_controller.rb
@@ -5,12 +5,8 @@ end
 
 
 get '/words/:word' do
-	@word = Word.find(params[:word])
+	@word = Word.find_by_text(params[:word])
 	erb :"/words/show"
 end
 
-get '/words/:id' do
-	@word = Word.find(params[:id])
-	erb :"/words/show"
-end
-
+		
