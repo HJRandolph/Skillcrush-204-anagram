de87cafd265e7eb4a33466da201c4bdb3dd13e6e
HJRandolph <hjrandolph@gmail.com>
2017-01-21 00:32:02 -0500

    fixed words_controller so that using an ID shows a word

--- app/controllers/words_controller.rb
+++ app/controllers/words_controller.rb
@@ -3,14 +3,12 @@ get '/words' do
 	erb :"/words/index"
 end
 
-get '/words/:word' do
-	@word = Word.find_by_text(params[:word])
-	erb :"/words/show"
-end
-
-
-
 get '/words/:id' do
 	@word = Word.find(params[:id])
 	erb :"/words/show"
 end	
+
+get '/words/:word' do
+	@word = Word.find_by_text(params[:word])
+	erb :"/words/show"
+end
